55d854042d2cad39ff5e4c4d6bc46bb19235c8d2
sicoyle <sic14@txstate.edu>
2017-02-22 22:15:47 -0600

    Read in world properly.

--- CS2308/Project3/life.cpp
+++ CS2308/Project3/life.cpp
@@ -20,9 +20,28 @@ using namespace std;
 
 int ROWS;         //stores the number of rows in the grid
 int COLUMNS;      //stores the number of rows in the grid
-
+char ** arr;
 //This function reads input file for subsequent prosessing (add high level 
 //description of your implementation logic) 
+void gridSize(const char * file)
+{
+	//Create file object, declare variables
+	ifstream fin(file);
+	char ch;
+	
+	//Read in the grid size
+	while(fin.get(ch))
+	{
+		COLUMNS++;
+		if(ch == '\n')
+			ROWS++;
+	}
+
+	//Get the correct column value
+	COLUMNS = COLUMNS/ROWS;
+	fin.close();
+}
+
 void populateWorld (const char * file)
 {
 	//Initialize variables
@@ -30,40 +49,55 @@ void populateWorld (const char * file)
 	COLUMNS = 0;
 	ifstream fin; //Create file object
 	
+	gridSize(file);
 	//Open file
 	fin.open(file);
 
 	//Rest of variables
-	int ** arr;
 	char ch;
+
+	arr = new char * [ROWS]; 
 
 	//Make sure file opens
 	if(fin.fail())
 		cout << "Error: input file did not open. ";
-cout << "HERE" << endl;		
-	//Read in input for each row of data
-	while(fin.get(ch))
-	{
-cout << "here";
-		COLUMNS++;
-		if(ch == '\n')
-			ROWS++;
-	}
-	COLUMNS = COLUMNS/ROWS;
 
+	for(int r = 0; r < ROWS; r++)
+	{
+		arr[r] = new char [COLUMNS];
+		cout << "arr "<< arr[r];
+	}
+
+	for(int row = 0; row < ROWS; row++)
+	{
+		for(int columns = 0; columns < COLUMNS; columns++)
+		{
+		fin.get(ch);
+		if(ch == 48 || ch-1 == 48)		
+			*(* (arr + row) + columns) = ch;
+		}
+	}
+
+	fin.close();
 }
 //This function outputs the grid for current generation (add high level 
 //description of your implementation logic) 
 void showWorld ()
 {
-	cout << "In showworld function\n";
-cout << ROWS << endl << COLUMNS;
+for(int r = 0; r < ROWS; r++)
+{
+	for(int c = 0; c < COLUMNS; c++)
+		cout << *(*(arr+r)+c);
+	cout << endl;
+}
+
+
+
 }
 
 //This function creats new geneneration grid from the old generation grid
 //(add high level description of your implementation logic)
 void iterateGeneration ()
 {
-	cout << "In iterategeneration function\n";
 }
 
