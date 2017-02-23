30a39c29244fd2efb809525889c9c6d5843083e7
sicoyle <sic14@txstate.edu>
2017-02-22 21:12:39 -0600

    Properly reading in file. Make reading into the file as a new function for clarity.

--- CS2308/Project3/life.cpp
+++ CS2308/Project3/life.cpp
@@ -31,38 +31,33 @@ void populateWorld (const char * file)
 	ifstream fin; //Create file object
 	
 	//Open file
-	fin.open("glider_gun_fight.txt");
+	fin.open(file);
 
 	//Rest of variables
 	int ** arr;
-	//arr = new int * [ROWS];
-	//char ch = new int [COLUMNS];
 	char ch;
-	vector < vector<char> > gameBoard; //For reading in the gameboard info	
 
 	//Make sure file opens
 	if(fin.fail())
 		cout << "Error: input file did not open. ";
-		
+cout << "HERE" << endl;		
 	//Read in input for each row of data
 	while(fin.get(ch))
 	{
-		if(atoi(ch) == 0 || atoi(ch) == 1)
-			gameBoard[ROWS];
-
-		if(ch == '\n')
-//This line needs fixing			gameBoard.push_back(vector <char>);
-		}
-		gameBoard.push_back(temp);
-		ROWS++;
-	}
+cout << "here";
 		COLUMNS++;
+		if(ch == '\n')
+			ROWS++;
+	}
+	COLUMNS = COLUMNS/ROWS;
+
 }
 //This function outputs the grid for current generation (add high level 
 //description of your implementation logic) 
 void showWorld ()
 {
 	cout << "In showworld function\n";
+cout << ROWS << endl << COLUMNS;
 }
 
 //This function creats new geneneration grid from the old generation grid
