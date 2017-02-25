3d0df077e7cd4e234ebd072824a8c2fe59c07277
sicoyle <sic14@txstate.edu>
2017-02-25 16:59:17 -0600

    Errors with count function. error on atoi lines.

--- CS2308/Project3/life.cpp
+++ CS2308/Project3/life.cpp
@@ -9,7 +9,6 @@ Instructor: Komogortsev, TSU
 
 //This file provides the implementation of the life.h header file.
 
-
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -21,7 +20,7 @@ using namespace std;
 
 int ROWS;         //stores the number of rows in the grid
 int COLUMNS;      //stores the number of rows in the grid
-char ** arr;
+char ** arr;      //Stores the grid values
 
 //This function reads input file for subsequent prosessing (add high level 
 //description of your implementation logic) 
@@ -30,7 +29,6 @@ void gridSize(const char * file)
 	//Create file object, declare variables
 	ifstream fin(file);
 	char ch;
-
 
 	if(fin.fail())
 		cout << "Error: input file did not open first try." << endl;
@@ -46,6 +44,49 @@ void gridSize(const char * file)
 	//Get the correct column value
 	COLUMNS = COLUMNS/ROWS;
 	fin.close();
+}
+
+int Count(int j, int k, char ** arr)
+{
+	//Declare variables
+	int counter = 0;
+
+	//Get neighbor counts for each cell
+	for(int m = j - 1; m <= j + 1; m++)
+	{
+		for(int n = k - 1; n <= k + 1; n++)
+		{
+			//Incremement count if cell is alive
+			counter += atoi(arr[m][n]);
+		}
+			//Subtract value of cell from NC
+			counter -= atoi(arr[j][k]);	
+		
+	}
+	//Return neighbor count
+	return counter;
+}
+
+void countNeighbors(char ** arr)
+{
+	//Create variable to hold the # of neighbors a cell has
+	char ** NC;
+
+	//Allocate memory for neighbor counts
+	NC = new char * [ROWS + 2];
+	for(int r = 0; r < (ROWS); r++)
+		NC[r] = new char [COLUMNS + 2];
+
+	//Assign number of neighbors to NC
+	for(int j = 0; j < ROWS; j++)
+	{
+		for(int k = 0; k < COLUMNS; k++)
+		{
+			NC[j][k] = Count(j,k,arr);
+		}
+	}	
+		
+	
 }
 
 void populateWorld (const char * file)
@@ -88,7 +129,9 @@ void populateWorld (const char * file)
 				*(* (arr + row) + columns) = ch;
 		}
 	}
-
+	
+	//countNeighbors(arr);	
+	
 	fin.close(); //Close the files
 }
 
@@ -105,19 +148,13 @@ void showWorld ()
 		}
 		cout << endl;
 	}
-}
-
-void CountNeighbors()
-{
-	cout << "Count Neighbors function. " << endl;	
-	
-	//
+	countNeighbors(arr);
 }
 //This function creats new geneneration grid from the old generation grid
 //(add high level description of your implementation logic)
 void iterateGeneration ()
 {
-	cout << "Iterate gen functin. " << endl;
+	cout << "Iterate gen function. " << endl;
 
 }
 
