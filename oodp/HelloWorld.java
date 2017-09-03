27b92f541fa9da5e61528456bb749f5f96da5018
sicoyle <sic14@txstate.edu>
2017-09-03 11:45:45 -0500

    First java program:)

--- oodp/HelloWorld.java
+++ oodp/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		//Print hellow wworld to terminal
+		//Print hello wworld to terminal
 		System.out.println("Hello world");
 	}
 }
