190c1ced40292b0263c2df64c43636ceb7548fff
sicoyle <sic14@txstate.edu>
2017-09-14 15:48:04 -0500

    Start of assignment 1

---
+++ oodp/ShippingStore.java
@@ -0,0 +1,8 @@
+/**
+	*This program simulates a shipping store database that allows users to maintain the packages within
+	*storage room.
+	*@author Samantha Coyle
+	*@author Cassandra Coyle
+**/
+
+
