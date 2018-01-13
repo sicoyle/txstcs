32760f5b234b27ba8801ccd2bc040a378ef6e3d5
sicoyle <sic14@txstate.edu>
2018-01-13 15:25:41 -0600

    Problem 1 solved:)

--- projectEuler/Problem_001.go
+++ projectEuler/Problem_001.go
@@ -1,5 +1,18 @@
 //Problem 1 on Project Euler
+//Multiples of 3 and 5
 //1/13/2018
 
+package main
+import "fmt"
 
+func main(){
+	sum := 0
 
+	for a := 1; a < 1000; a++ {
+		if (a % 3 == 0) || (a % 5 == 0){
+			sum += a
+			}
+	}
+
+	fmt.Println(sum);
+}
